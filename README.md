6b2e6c777a8c8f8cad0b02b38ad61ac2068c5930
stephenw987 <stephenw987@gmail.com>
2024-07-02 00:35:33 -0400

    Add files via upload
    try #9 to push to repo. This time is manually through the browser

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# Task-Board
-Challenge05
+# Personal_Blog-Challenge05 Task-Board
+
+
+## Description
+![Screenshot](assets/images/screenshot1.png)
+
+This is a simple task board application to manage project tasks by modifying starter code. it will run in the browser while dynamically updating HTML and CSS by jQuery.
+
+## Usage
+
+For a project team member assigned with multiple tasks to organize, this is a task board for adding individual project tasks, managing their state of progress and tracking their overall project progress accordingly.
+## Credits
+
+code source: https://github.com/coding-boot-camp/musical-happiness
+
+
+## License
+
+MIT License
+
+Copyright (c) [2024] [Stephen Wei]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ assets/js/script.js
@@ -0,0 +1,144 @@
+// Retrieve tasks and nextId from localStorage
+let taskList = JSON.parse(localStorage.getItem("tasks"));
+let nextId = JSON.parse(localStorage.getItem("nextId"));
+
+// Todo: create a function to generate a unique task id
+function generateTaskId() {
+  if (nextId === null) {
+    nextId = 1;
+  } else {
+    nextId++;
+  }
+  localStorage.setItem("nextId", JSON.stringify(nextId));
+  return nextId;
+}
+
+// Todo: create a function to create a task card
+function createTaskCard(task) {
+  const taskCard = $('<div>')
+    .addClass('card w-75 task-card draggable my-3')
+    .attr('data-task-id', task.id);
+  const cardHeader = $('<div>').addClass('card-header h4').text(task.title);
+  const cardBody = $('<div>').addClass('card-body');
+  const cardDescription = $('<p>').addClass('card-text').text(task.description);
+  const cardDueDate = $('<p>').addClass('card-text').text(task.dueDate);
+  const cardDeleteBtn = $('<button>')
+    .addClass('btn btn-danger delete')
+    .text('Delete')
+    .attr('data-task-id', task.id);
+  cardDeleteBtn.on('click', handleDeleteTask);
+
+  if (task.dueDate && task.status !== 'done') {
+    const now = dayjs();
+    const taskDueDate = dayjs(task.dueDate, 'DD/MM/YYYY');
+    if (now.isSame(taskDueDate, 'day')) {
+      taskCard.addClass('bg-warning text-white');
+    } else if (now.isAfter(taskDueDate)) {
+      taskCard.addClass('bg-danger text-white');
+      cardDeleteBtn.addClass('border-light');
+    }
+  }
+
+  cardBody.append(cardDescription, cardDueDate, cardDeleteBtn);
+  taskCard.append(cardHeader, cardBody);
+
+  return taskCard;
+}
+
+// Todo: create a function to render the task list and make cards draggable
+function renderTaskList() {
+    if (!taskList) {
+        taskList = [];
+    }
+    const todoList = $('#todo-cards');
+    todoList.empty();
+
+    const inProgressList = $('#in-progress-cards');
+    inProgressList.empty();
+
+    const doneList = $('#done-cards')
+    doneList.empty();
+
+    for (let task of taskList) {
+        if (task.status === 'to-do') {
+            todoList.append(createTaskCard(task));
+        } else if (task.status === 'in-progress') {
+            inProgressList.append(createTaskCard(task));
+        } else if (task.status === 'done') {
+            doneList.append(createTaskCard(task));
+        }
+    }
+
+    $('.draggable').draggable({
+        opacity: 0.7,
+        zIndex: 100,
+        helper: function (e) {
+            const original = $(e.target).hasClass('ui-draggable')
+                ? $(e.target)
+                : $(e.target).closest('.ui-draggable');
+            return original.clone().css({
+                maxWidth: original.outerWidth(),
+            });
+        },
+    });
+}
+
+// Todo: create a function to handle adding a new task
+function handleAddTask(event) {
+  event.preventDefault();
+
+  const task = {
+    id: generateTaskId(),
+    title: $('#taskTitle').val(),
+    description: $('#taskDueDate').val(),
+    status: 'to-do',
+  };
+
+  taskList.push(task);
+  localStorage.setItem('tasks', JSON.stringify(taskList));
+  renderTaskList();
+  $('#taskTitle').val('');
+  $('#taskDescription').val('');
+  $('#taskDueDate').val('');
+}
+
+// Todo: create a function to handle deleting a task
+function handleDeleteTask(event) {
+  event.preventDefault();
+  const taskId = $(this).attr('data-task-id');
+
+  taskList = taskList.filter((task) => task.id !== parseInt(taskId));
+  localStorage.setItem('tasks', JSON.stringify(taskList));
+  renderTaskList();
+}
+
+// Todo: create a function to handle dropping a task into a new status lane
+function handleDrop(event, ui) {
+
+  const taskId = ui.draggable[0].dataset.taskId;
+  const newStatus = event.target.id;
+
+  for (let task of taskList) {
+
+    if (task.id === parseInt(taskId)) {
+      task.status = newStatus;
+    }
+  }
+
+  localStorage.setItem('tasks', JSON.stringify(taskList));
+  renderTaskList();
+}
+
+// Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
+$(document).ready(function () {
+  renderTaskList();
+  $('#TaskForm').on('submit', handleAddTask);
+$('.lane').droppable({
+    accept: '.draggable',
+    drop: handleDrop,
+  });
+  $('#taskDueDate').datepicker({
+    changeMonth: true,
+    changeYear: true,
+  });
+});
